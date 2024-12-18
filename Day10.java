9ecd20859ddc67a33328686a1ad1dee769e8a106
Andrew Li <andrewjli@utexas.edu>
2024-12-18 13:08:34 -0600

    Day10 Part2

--- Day10.java
+++ Day10.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class Day10 {
 
 
-    static boolean[][] visited;
+    // static boolean[][] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("Day10.in"));
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("Day10.out")));
@@ -19,7 +19,7 @@ public class Day10 {
         int[][] rslt = new int[input.size()][input.get(0).length()];
         for (int i = 0; i < input.size(); i++) {
             for (int j = 0; j < input.get(i).length(); j++) {
-                visited = new boolean[input.size()][input.get(0).length()];
+                // visited = new boolean[input.size()][input.get(0).length()];
                 if (input.get(i).charAt(j) == '0') rslt[i][j] += dfs(i, j, input, 0); 
             }
         }
@@ -40,8 +40,8 @@ public class Day10 {
         if (i < 0 || i >= input.size() || j < 0 || j >= input.get(0).length()) return 0;
         if (input.get(i).charAt(j) - '0' == curr) {
             if (curr == 9) {
-                if (visited[i][j]) return 0;
-                visited[i][j] = true;
+                // if (visited[i][j]) return 0;
+                // visited[i][j] = true;
                 return 1;
             }
             else {
